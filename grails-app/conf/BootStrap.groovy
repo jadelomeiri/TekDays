7c2f51c6037c683a9b04020e5c1c2a068bfde680
jadelomeiri <jad.elomeiri@gmail.com>
2016-09-11 18:34:55 +0100

    first domain class (TekEvent) + scaffolded controller + bootsrapped data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,35 @@
+import com.tekdays.TekEvent
+
 class BootStrap {
 
     def init = { servletContext ->
+		def event1 = new TekEvent(name: 'Java Code Camp',
+							city: 'San Francisco',
+							organizer: 'John Doe',
+							venue: 'TBD',
+							startDate: new Date('11/27/2016'),
+							endDate: new Date('11/28/2016'),
+							description: '''a code camp for java devs of all levels.
+										Join us for free. For more details contact me
+										at me@johndoe.com''')
+		if(!event1.save()) {
+			event1.errors.allErrors.each {error ->
+				println "An error occurend with event1: ${error}"
+			}
+		}
+		
+		def event2 = new TekEvent(name: 'Groovy days',
+			city: 'New York City',
+			organizer: 'Adam Smith',
+			venue: 'The Apprentice Venue',
+			startDate: new Date('12/06/2016'),
+			endDate: new Date('12/06/2016'),
+			description: "description not yet available")
+		if(!event2.save()) {
+			event2.errors.allErrors.each {error ->
+				println "An error occurend with event2: ${error}"
+			}
+		}
     }
     def destroy = {
     }
